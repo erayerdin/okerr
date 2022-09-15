e915b1017e38804db0c6182bebc57c7848dc9022
Eray Erdin <eraygezer.94@gmail.com>
2022-09-16 02:24:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Okerr
+
+Okerr is a Dart package imitating [Rust's results](https://doc.rust-lang.org/std/result/index.html).
---
+++ test/okerr_test.dart
@@ -0,0 +1,14 @@
+void main() {
+  // TODO write a test
+  // group('A group of tests', () {
+  //   final awesome = Awesome();
+
+  //   setUp(() {
+  //     // Additional setup goes here.
+  //   });
+
+  //   test('First Test', () {
+  //     expect(awesome.isAwesome, isTrue);
+  //   });
+  // });
+}
