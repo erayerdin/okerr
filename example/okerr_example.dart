fc23dbce364e9f919e3a0378a8f2e60e315b3112
Eray Erdin <eraygezer.94@gmail.com>
2022-09-16 14:47:01 +0300

    add example code

--- example/okerr_example.dart
+++ example/okerr_example.dart
@@ -1,3 +1,34 @@
+// ignore_for_file: avoid_print
+
+import 'package:okerr/okerr.dart';
+
+enum VoterFailure { tooYoung, tooOld }
+
+Result<bool, VoterFailure> canVote(int age) {
+  if (age < 0) {
+    return Err(VoterFailure.tooYoung);
+  }
+
+  if (age > 150) {
+    return Err(VoterFailure.tooOld);
+  }
+
+  return Ok(age >= 18);
+}
+
 void main() {
-  // TODO write an example
+  const burton = 10;
+  const kendrick = 20;
+  const shae = -1;
+  const calvin = 200;
+
+  final canBurtonVote = canVote(burton);
+  final canKendrickVote = canVote(kendrick);
+  final canShaeVote = canVote(shae);
+  final canCalvinVote = canVote(calvin);
+
+  print(canBurtonVote);
+  print(canKendrickVote);
+  print(canShaeVote);
+  print(canCalvinVote);
 }
